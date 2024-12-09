ae5d97225cde7109e6ad62eaabb1aa25b9b6d858
Rahim Prasla <rahim.prasla@gmail.com>
2024-12-09 19:55:00 +0000

    added src

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const cors = require("cors");
 const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "jobs.json"));
+const router = jsonServer.router(path.join(__dirname, "src", "jobs.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
